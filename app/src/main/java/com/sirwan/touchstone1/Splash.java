7818f19ff295aaa8d0e73f5d24489319a63fdaff
SIrwan Rahimi <sirwanrahimi2020@gmail.com>
2019-10-08 01:48:01 +0330

    splash is v3 with fullscreen style and main activity changed to no action bar fullscreen+ bottom navigation action bar color red===> to splash screen

--- app/src/main/java/com/sirwan/touchstone1/Splash.java
+++ app/src/main/java/com/sirwan/touchstone1/Splash.java
@@ -1,8 +1,10 @@
 package com.sirwan.touchstone1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
@@ -12,6 +14,10 @@ Thread thread;
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_splash);
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.color_new_red));
+        }
         thread = new Thread(new Runnable()
         {
             @Override
