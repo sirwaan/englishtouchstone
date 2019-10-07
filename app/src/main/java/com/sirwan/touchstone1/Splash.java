843619d233e6afc80ae31fe8a9fb8a61a0e8974b
SIrwan Rahimi <sirwanrahimi2020@gmail.com>
2019-10-08 01:00:14 +0330

    add splash screen v1

--- app/src/main/java/com/sirwan/touchstone1/Splash.java
+++ app/src/main/java/com/sirwan/touchstone1/Splash.java
@@ -2,13 +2,32 @@ package com.sirwan.touchstone1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
-
+Thread thread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        thread = new Thread(new Runnable()
+        {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    Intent intent = new Intent(Splash.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();  // if we want not to go back to splash screen again by press back , we should finish it
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+
     }
 }
